537f285b0ab81c1487b6d7c9ff442af130f2e5f7
devbok0101 <devbok0101@gmail.com>
2024-10-17 01:13:44 +0900

    [ADD] LeetCode Combinations 백트래킹 사용 버전 추가

--- 1week/combinations/combinations_itertools.py
+++ 1week/combinations/combinations_itertools.py
@@ -5,7 +5,19 @@ from typing import List
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
 
-        return list(itertools.combinations(range(1, n + 1), k))
+        def backTrack(start, path):
+            if len(path) == k:
+                ans.append(path[:])
+                return
+
+            for i in range(start, n + 1):
+                path.append(i)
+                backTrack(i + 1, path)
+                path.pop()
+
+        ans = []
+        backTrack(1, [])
+        return ans
 
 
 print(Solution().combine(4, 2))
