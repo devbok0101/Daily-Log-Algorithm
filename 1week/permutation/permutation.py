e9192add2314773f941a5cbf7fde83c1c3c93c35
devbok0101 <devbok0101@gmail.com>
2024-10-17 00:55:17 +0900

    [ADD] LeetCode Permutation 백트래킹 버전 추가

--- 1week/permutation/permutation.py
+++ 1week/permutation/permutation.py
@@ -3,6 +3,20 @@ from typing import List
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return []
+
+        def backTrack(curr):
+            if len(curr) == len(nums):
+                ans.append(curr[:])
+                return
+
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backTrack(curr)
+                    curr.pop()
+
+        ans = []
+        backTrack([])
+        return ans
 
 print(Solution().permute([1,2,3]))
