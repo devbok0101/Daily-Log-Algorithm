a662d3772d296607fc710652df68ee5a514e3f34
devbok0101 <devbok0101@gmail.com>
2024-10-17 01:06:20 +0900

    [ADD] LeetCode Combinations itertools 사용 버전 추가

---
+++ 1week/combinations/combinations_itertools.py
@@ -0,0 +1,11 @@
+import itertools
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        return list(itertools.combinations(range(1, n + 1), k))
+
+
+print(Solution().combine(4, 2))
