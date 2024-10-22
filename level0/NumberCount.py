fdd7a5187d4d609ed12216adca747d4ea830e03d
devbok0101 <devbok0101@gmail.com>
2024-10-22 21:40:14 +0900

    [ADD] 프로그래머스 짝수 홀수 개수 세기 문제 풀이 추가

--- level0/NumberCount.py
+++ level0/NumberCount.py
@@ -3,6 +3,18 @@
 
 def solution(num_list):
     answer = []
+
+    even_num_count = 0
+    odd_num_count = 0
+
+    for num in num_list:
+        if num % 2 == 0:
+            even_num_count += 1
+        else:
+            odd_num_count += 1
+
+    answer = [even_num_count, odd_num_count]
+
     return answer
 
 
