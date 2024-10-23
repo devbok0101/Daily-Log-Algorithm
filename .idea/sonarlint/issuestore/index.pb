790c971933c8c06649d03278cdde1fa96a1a2669
devbok0101 <devbok0101@gmail.com>
2024-10-23 21:10:00 +0900

    [ADD] palindorme 문제 추가

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+Z
+*1week/palindrome/PalindromePartitioning.py,5/7/57b304e41bbe88d32eb6e0506ea312443bf0c4a8
---
+++ 1week/palindrome/PalindromePartitioning.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        return ["a"]
+
+
+print(Solution.partition(Solution,"a"))
