41d0970bcb5dba135c612f2b64423dbfe034203d
devbok0101 <devbok0101@gmail.com>
2024-10-24 21:10:00 +0900

    [ADD] palindorme 문제 풀이 추가

--- 1week/palindrome/PalindromePartitioning.py
+++ 1week/palindrome/PalindromePartitioning.py
@@ -3,7 +3,24 @@ from typing import List
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        return ["a"]
+        if not s:
+            return []
+        result = []
+
+        def back_track(start, partition):
+            if len(s) == start:
+                result.append(partition[:])
+                return
+
+            for i in range(start + 1, len(s) + 1):
+                slicing = s[start:i]
+                if slicing == slicing[::-1]:
+                    partition.append(slicing)
+                    back_track(i, partition)
+                    partition.pop()
+
+        back_track(0, [])
+        return result
 
 
-print(Solution.partition(Solution,"a"))
+print(Solution.partition(Solution, "aab"))
