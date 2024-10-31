d98a39ce1b63e30126f96e15b20ec949fb47af7d
devbok0101 <devbok0101@gmail.com>
2024-10-31 21:46:26 +0900

    [ADD] Leetcode Daily Temperatures 문제 풀이 추가

---
+++ 2week/dailyTemperatures/DailyTemperatures.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        return []
+
+
+print(Solution.dailyTemperatures([73,74,75,71,69,72,76,73]))
