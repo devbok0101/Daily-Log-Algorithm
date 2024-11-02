1cb9eeaa62cf5f891ff2a29fa7b409b52a8b02f1
devbok0101 <devbok0101@gmail.com>
2024-11-02 21:13:10 +0900

    [ADD] dailyTemperatures for 문으로 더 큰 온도 찾기 어려움 봉착

--- 2week/dailyTemperatures/DailyTemperatures.py
+++ 2week/dailyTemperatures/DailyTemperatures.py
@@ -1,11 +1,27 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        start, last = 0, len(temperatures)
+        answer = [0 for _ in range(last)]
+
+        stack = []
+        while last - 1 >= start:
+            stack.append(temperatures[last - 1])
+            last -= 1
+
+        for index in range(len(temperatures)):
+            nowTemp = temperatures[index]
+            for i in range(index + 1):
+                stack.pop()
+            nextTemp = stack[0]
 
 
-        return []
 
 
-print(Solution.dailyTemperatures([73,74,75,71,69,72,76,73]))
+        return answer
+
+
+print(Solution.dailyTemperatures(Solution, [73,74,75,71,69,72,76,73]))
