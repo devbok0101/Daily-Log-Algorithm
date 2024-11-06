5b84e0dff8f1f860e9e6decc0ae852e38c245bd5
devbok0101 <devbok0101@gmail.com>
2024-11-06 21:08:57 +0900

    [ADD] leetCode TrappingRainWater 스택으로 풀기

--- 2week/trappingRainWater/TrappingRainWater.py
+++ 2week/trappingRainWater/TrappingRainWater.py
@@ -3,7 +3,28 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        return 0
+        answer = 0
+        stack = []
+        trapped = height[:]
+        popElement = (0,0)
+
+        for i, h in enumerate(height):
+            while stack and h >= stack[-1][1]:
+                popElement = stack.pop()
+            if stack:
+                left = stack[-1]
+                for j in range(left[0] + 1, i):
+                    trapped[j] = h
+            else:
+                left = popElement
+                for j in range(left[0] +1, i):
+                    trapped[j] = left[1]
+            stack.append((i, h))
+
+        for i in range(len(height)):
+            diff = trapped[i] - height[i]
+            answer += diff
+        return answer
 
 
 
