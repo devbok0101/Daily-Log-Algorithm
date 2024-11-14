c277ed3ba0bfc05b7607740819939978bcd4035d
devbok0101 <devbok0101@gmail.com>
2024-11-14 23:15:10 +0900

    [ADD] 프로그래머스 level 0 짝수는 싫어요 문제 풀이 추가

--- level0/HateEvneNum.py
+++ level0/HateEvneNum.py
@@ -2,6 +2,11 @@
 
 def solution(n):
     answer = []
+
+    for i in range(1, n + 1):
+        if i % 2 != 0:
+            answer.append(i)
+
     return answer
 
-print(solution(10))
+print(solution(1))
