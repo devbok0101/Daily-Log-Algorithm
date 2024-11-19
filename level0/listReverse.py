23f299e67598efa660204c698a6a84b2c37fcea8
devbok0101 <devbok0101@gmail.com>
2024-11-19 22:39:44 +0900

    [ADD] 프로그래머스 리스트 뒤집기 풀이방법 2 슬라이싱 적용하기

--- level0/listReverse.py
+++ level0/listReverse.py
@@ -1,11 +1,5 @@
 def solution(num_list):
-    answer = []
-    num_len = len(num_list) - 1
-    while num_len != -1:
-        answer.append(num_list[num_len])
-        num_len -= 1
-
-    return answer
+    return num_list[::-1]
 
 
 print(solution([1, 2, 3, 4, 5]))
