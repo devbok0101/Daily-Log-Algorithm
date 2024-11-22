d34a737b0df1b6a692842bfc7d3b367fb0e17680
devbok0101 <devbok0101@gmail.com>
2024-11-22 21:30:14 +0900

    [ADD] 프로그래머스 level 0 배열 자르기 문제 풀이 다른 방법 추가

--- level0/Cut_list.py
+++ level0/Cut_list.py
@@ -1,15 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120833
 
 def solution(numbers, num1, num2):
-    answer = []
-
-    for index, num in enumerate(numbers):
-        if index == num1 and index <= num2:
-            answer.append(num)
-            num1 += 1
-
-
-    return answer
+    return numbers[num1: num2 + 1]
 
 
 print(solution([1, 2, 3, 4, 5], 1, 3))
