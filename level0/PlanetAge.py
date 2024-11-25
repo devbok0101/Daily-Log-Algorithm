09b1ca90b57fc815b83791e9d7f6a773e50f4fdd
devbok0101 <devbok0101@gmail.com>
2024-11-25 21:30:14 +0900

    [ADD] 프로그래머스 level 0 외계 행성의 나이 문제 풀이 추가

--- level0/PlanetAge.py
+++ level0/PlanetAge.py
@@ -1,7 +1,19 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/120834
+# https://school.programmers.co.kr/learn/courses/30/lessons/120834
+from collections import defaultdict
+
 
 def solution(age):
     answer = ''
+    ans = defaultdict()
+    spell = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+    for i in range(0, 10):
+        ans[i] = spell[i]
+
+    for num in str(age):
+        answer += spell[int(num)]
+
     return answer
+
 
 print(solution(23))
