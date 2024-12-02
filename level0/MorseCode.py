1bfb6ee48936a60c1a8bad1dd499b307d67003ad
devbok0101 <devbok0101@gmail.com>
2024-12-02 22:57:20 +0900

    [ADD] 프로그래머스 level 0 모스부호 문제 풀이 추가

--- level0/MorseCode.py
+++ level0/MorseCode.py
@@ -1,8 +1,16 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120838
 
 def solution(letter):
-    answer = ''
-    return answer
+
+    morse = {
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+
+    return ''.join(morse[code] for code in letter.split(" "))
 
 
 print(solution(".... . .-.. .-.. ---"))
