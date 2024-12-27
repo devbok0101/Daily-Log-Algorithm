f70a915ba5895d24223a46fa748a4bd9f5665c37
devbok0101 <devbok0101@gmail.com>
2024-12-27 21:30:14 +0900

    [ADD] 프로그래머스 level 0 가까운 수 풀이 해결

--- level0/nearCount.py
+++ level0/nearCount.py
@@ -1,5 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120890
 
 def solution(array, n):
-    answer = 0
+    answer = array[0]
+    for number in array:
+        if abs(n - answer) > abs(n - number):
+            answer = number
+        if abs(n - answer) == abs(n - number):
+            answer = min(answer, number)
     return answer
+
+print(solution([3, 10, 28], 20))
+print(solution([4, 7], 1))
+print(solution([10, 11, 12], 13))
