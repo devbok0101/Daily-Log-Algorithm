24c7699f8f6cc3dbb5a1ab247676011d46ca497c
devbok0101 <devbok0101@gmail.com>
2024-12-31 21:30:14 +0900

    [ADD] 프로그래머스 level 0 369 문제 풀이 완

--- level0/369games.py
+++ level0/369games.py
@@ -1,5 +1,15 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/120891
 
+def isNumIn369(num):
+    return num == 3 or num == 6 or num == 9
+
+
 def solution(order):
+    numbers = list(map(int, str(order)))
     answer = 0
+    for num in numbers:
+        if isNumIn369(int(num)):
+            answer += 1
     return answer
+
+print(solution(29423))
